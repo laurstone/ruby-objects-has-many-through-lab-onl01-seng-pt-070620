e828ae6ee41e776c698f0eb83cd0186cd291429a
Lauren Stone <lstone321@gmail.com>
2020-08-13 03:22:01 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,12 +1,29 @@
-class Patient
   
-  attr_accessor :name, :appointment
+class Patient
+  attr_accessor :name
+ 
+  @@all=[]
   
   def initialize(name)
     @name = name 
-  end 
-  
+    @@all << self 
+  end
+
   def new_appointment(date, doctor)
-    
-  
-end 
+    Appointment.new(date, self, doctor)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+  end 
+  end
+
+  def doctors
+    self.appointments.collect {|appointment| appointment.doctor}
+  end 
+end
